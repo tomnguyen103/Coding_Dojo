d0d398cc09063ecaa321449520db1deaa11586ff
tomnguyen103 <huuthong103@gmail.com>
2019-10-02 14:08:10 -0700

    oct 2

--- python_stack/python/fundamentals/function2.py
+++ python_stack/python/fundamentals/function2.py
@@ -1,41 +1,41 @@
-# x = [ [5,2,3], [10,8,9] ] 
-# students = [
-#      {'first_name':  'Michael', 'last_name' : 'Jordan'},
-#      {'first_name' : 'John', 'last_name' : 'Rosales'}
-# ]
-# sports_directory = {
-#     'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
-#     'soccer' : ['Messi', 'Ronaldo', 'Rooney']
-# }
-# z = [ {'x': 10, 'y': 20} ]
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
 
-# # 1.
-# x[1][0] = 15
-# print(x)
+# 1.
+x[1][0] = 15
+print(x)
 
-# # 2.
-# print(students[0]['last_name'])
-# students[0]['last_name'] = 'Bryant'
-# print(students[0]['last_name'])
+# 2.
+print(students[0]['last_name'])
+students[0]['last_name'] = 'Bryant'
+print(students[0]['last_name'])
 
-# # 3.
-# print(sports_directory['soccer'][0])
-# sports_directory['soccer'][0] = 'Andres'   
-# print(sports_directory['soccer'][0])
+# 3.
+print(sports_directory['soccer'][0])
+sports_directory['soccer'][0] = 'Andres'   
+print(sports_directory['soccer'][0])
 
-# # 4.
-# print(z[0]['y'])
-# z[0]['y']= 30
-# print(z[0]['y'])
+# 4.
+print(z[0]['y'])
+z[0]['y']= 30
+print(z[0]['y'])
 
 
 ###########################################
-# students = [
-#          {'first_name':  'Michael', 'last_name' : 'Jordan'},
-#          {'first_name' : 'John', 'last_name' : 'Rosales'},
-#          {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-#          {'first_name' : 'KB', 'last_name' : 'Tonel'}
-#     ]
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
 
 # should output: (it's okay if each key-value pair ends up on 2 separate lines;
 # bonus to get them to appear exactly as below!)
@@ -44,11 +44,11 @@
 # first_name - Mark, last_name - Guillen
 # first_name - KB, last_name - Tonel
 
-# def iterateDictionary(some_list):
-#     for i in some_list:
-#         print("first_name" , "-" , i["first_name"],",","last_name", "-" , i["last_name"])
+def iterateDictionary(some_list):
+    for i in some_list:
+        print("first_name" , "-" , i["first_name"],",","last_name", "-" , i["last_name"])
 
-# iterateDictionary(students) 
+iterateDictionary(students) 
 
 
         # for key,val in some_list.items():
@@ -78,24 +78,20 @@
         #     print(j.keys())
         # for j in i.items():
         #     print(j.keys())
-
-
-
 # print(students[0]['first_name'])
 # print(students[0].keys())
-
 # print(students[0].keys())
 
 # 3.
-# def iterateDictionary2(key_name,some_list):
-#     for i in some_list:
-#         #print(i)
-#         #print(i.keys())
-#         #print(i.values())
-#         print(i[key_name])  
+def iterateDictionary2(key_name,some_list):
+    for i in some_list:
+        #print(i)
+        #print(i.keys())
+        #print(i.values())
+        print(i[key_name])  
 
-# iterateDictionary2('first_name', students)
-# iterateDictionary2('last_name', students)
+iterateDictionary2('first_name', students)
+iterateDictionary2('last_name', students)
 
 # 4.
 dojo = {
@@ -133,6 +129,3 @@ printInfo(dojo)
 # Patrick
 # Minh
 # D
-
-
-
