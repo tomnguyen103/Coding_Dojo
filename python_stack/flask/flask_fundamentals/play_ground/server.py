74c50063a4fbcb85dd20493985db9f71a50d4835
tomnguyen103 <huuthong103@gmail.com>
2019-10-03 17:40:02 -0700

    play ground

--- python_stack/flask/flask_fundamentals/play_ground/server.py
+++ python_stack/flask/flask_fundamentals/play_ground/server.py
@@ -4,15 +4,18 @@ app = Flask(__name__)
 
 @app.route("/play")
 def app_work():
-    return render_template("index.html")
+    return render_template("index.html",some_times=3)
 
-@app.route("/play/{times}")
+@app.route("/play/<times>")
 def more_grid(times):
-    return render_template("index.html",time=times)
+    #print('in times function')
+    return render_template("index.html",some_times=int(times))
+    
 
-@app.route("/play/{times}/{color}")
+@app.route("/play/<times>/<color>")
 def change_color(times,color):
-    return render_template("index.html",time=times,color=colors)
+    # print("inside the function change_color")
+    return render_template("index.html",some_times=int(times),some_color=color)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- python_stack/python/fundamentals/store_products/store.py
+++ python_stack/python/fundamentals/store_products/store.py
@@ -1,16 +1,28 @@
 class Store:
-    def __init__(self, name, product):
+    def __init__(self, name):
         self.name = name
-        self.product = Product(product.name,product.price,product.category)
+        self.productList = []
     def add_product(self,new_product):
-        self.product += new_product
+        self.productList.append(new_product)
     def sell_product(self,id):
-        self.product -= id
+        product_to_sell = self.productList[id]
+        self.productList.pop(id)
+        product_to_sell.print_infor()
+
     def inflation(self,percent_increase):
-        self.product.price *= percent_increase
+        for i in range(len(self.productList)):
+            self.productList[i].update_price(percent_increase,True)
+
     def set_clearance(self,category,percent_discount):
-        if self.product.category == category:
-            self.product.price *=percent_discount
+        for i in range(len(self.productList)):
+            single_product = self.productList[i]
+            if single_product.category == category:
+                single_product.update_price(percent_discount,False)
+
+    def print_info(self):
+        for product in self.productList:
+            print(product)
+
 class Product:
     def __init__(self,name,price,category):
         self.name = name
@@ -18,6 +30,26 @@ class Product:
         self.category = category
     def update_price(self, percent_change, is_increased):
         if is_increased is True:
-            self.price = percent_change * self.price
+            self.price *= 1 + (percent_change/100)
+        else:
+            self.price *= 1 - (percent_change/100)
     def print_infor(self):
         print("Product: {}, Category: {}, Price; {}".format(self.name,self.category,self.price))
+    def __str__(self):
+        return "Name: " + self.name
+
+
+store1 = Store("Malph's")
+
+store1.add_product(Product("hair spray", 5.75, "cosmetic"))
+store1.add_product(Product("banana",1.25, "grocery"))
+store1.add_product(Product("apple", 6, "grocery"))
+
+rotten_fruit = Product("rotten fruit", 10, "grocery")
+store1.add_product(rotten_fruit)
+
+store1.set_clearance("grocery", 50)
+store1.productList[2]
+store1.sell_product(3)
+
+store1.print_info()
