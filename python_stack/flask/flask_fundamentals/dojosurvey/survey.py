0f3b0f4e80ce74db1185c11671548ebd7cf0d672
Tom Nguyen <huuthong103@gmail.com>
2019-10-06 14:34:01 -0700

    survey

--- python_stack/flask/flask_fundamentals/dojosurvey/survey.py
+++ python_stack/flask/flask_fundamentals/dojosurvey/survey.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
     return render_template("index.html")
+
+@app.route('/users', methods = ['POST'])
+def create_user():
+    print("GOT POST INFOR!")
+    print(request.form)
+    name_from_form = request.form['name']
+    location_from_form = request.form['location']
+    language_from_form = request.form['language']
+    comment_from_form = request.form['comment']
+    return render_template('result.html',name_on_template = name_from_form,location_on_template=location_from_form, language_on_template=language_from_form,comment_on_template=comment_from_form)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
