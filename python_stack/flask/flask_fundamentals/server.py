e5a1b39b82903a62c991dced667e70c8c5c6b888
tomnguyen103 <huuthong103@gmail.com>
2019-10-07 12:08:28 -0700

    dojo_fruit_store"

--- python_stack/flask/flask_fundamentals/server.py
+++ python_stack/flask/flask_fundamentals/server.py
@@ -1,16 +1,33 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, session
 
 app = Flask(__name__)
 
+app.secret_key = "some really secret key"
+
+@app.route("/reset")
+def reset():
+    session.clear()
+    return redirect("/")
+
 @app.route("/process",methods=["GET","POST"])
 def process():
-    print(request.args.get("first_name"))
-    print(request.form)
+    # print(request.args.get("first_name"))
+    # print(request.form)
+
+    form = request.form
+
+    session["last_added_user"] = {
+        "first_name" : form["first_name"],
+        "last_name" : form["last_name"],
+        "hair_color" : form["hair_color"]
+    }
     return redirect("/")
-    # return "Hello"
 
 @app.route("/")
 def index():
+    if "last_added_user" not in session:
+        session["last_added_user"] = None
+    
     users = [{
         "first_name" : "Mark",
         "last_name" : "Lopez",
@@ -21,7 +38,7 @@ def index():
         "last_name" : "Peterson",
         "hair_color" : "dark mustard"
     }]
-    return render_template("index.html", cohor_mates=users)
+    return render_template("index.html", cohor_mates=users, last_added_user=session["last_added_user"] )
 
 
 if __name__ == "__main__": #running directly
