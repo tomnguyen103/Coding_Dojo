31ccfc68be89440dc850fb0143d44b11997671c8
Tom Nguyen <huuthong103@gmail.com>
2019-10-06 17:35:00 -0700

    Dojo Survey App

--- python_stack/flask/flask_fundamentals/dojosurvey/survey.py
+++ python_stack/flask/flask_fundamentals/dojosurvey/survey.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def main():
     return render_template("index.html")
 
-@app.route('/users', methods = ['POST'])
+@app.route('/results', methods = ['POST'])
 def create_user():
     print("GOT POST INFOR!")
     print(request.form)
@@ -15,7 +15,6 @@ def create_user():
     language_from_form = request.form['language']
     comment_from_form = request.form['comment']
     return render_template('result.html',name_on_template = name_from_form,location_on_template=location_from_form, language_on_template=language_from_form,comment_on_template=comment_from_form)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
