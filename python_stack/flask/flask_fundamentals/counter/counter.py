419253486378cfb6c54ad7318d3ca2496ad60217
tomnguyen103 <huuthong103@gmail.com>
2019-10-07 14:40:43 -0700

    sessions

--- python_stack/flask/flask_fundamentals/counter/counter.py
+++ python_stack/flask/flask_fundamentals/counter/counter.py
@@ -1,11 +1,28 @@
 from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
-app.secret_key = "keep it secret, keep it safe!"
+
+app.secret_key = "some secrect keys"
+
+app.count = 0
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if 'count' not in session:
+        session['count'] = 0
+
+    session['count'] += 1
+    return render_template('index.html', counter = session['count'])
+
+@app.route('/add_counter', methods = ['POST'])
+def add_counter():
+    session['count'] += 1
+    return redirect('/')
+
+@app.route('/destroy_session',methods = ['POST'])
+def destroy():
+    session.clear()
+    return redirect('/')
 
 
 if __name__=="__main__":   
