f3aec6c8f4926ddc237e428e025621cbab525469
Tom Nguyen <huuthong103@gmail.com>
2019-10-10 16:35:27 -0700

    model

---
+++ python_stack/django/django_orm/first_orm/apps/movie_app/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Movie(models.Model):
+    #id 
+    title = models.CharField(max_length=45)
+    description = models.TextField()
+    release_date = models.DateTimeField()
+    duration = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __repr__(self):
+        return f"Movie:{self.title} ({self.release_date})"
