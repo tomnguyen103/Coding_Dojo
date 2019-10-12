acf636ed7ca435d2634d605acc25e1ddfc092198
tomnguyen103 <huuthong103@gmail.com>
2019-10-12 09:39:07 -0700

    update

--- python_stack/django/django_orm/first_orm/apps/movie_app/views.py
+++ python_stack/django/django_orm/first_orm/apps/movie_app/views.py
@@ -4,6 +4,7 @@ from .models import Movie
 # Create your views here.
 def index(request):
     context = {
-        "all_movies" : Movie.objects.all()
+        "all_movies" : Movie.objects.all(),
+
     }
     return render(request,"movie_app/index.html",context)
