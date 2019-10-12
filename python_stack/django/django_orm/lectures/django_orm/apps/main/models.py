d8a4565f0ebca71ba7c1b78dff1a32d68c2ad3b1
tomnguyen103 <huuthong103@gmail.com>
2019-10-12 04:19:09 -0700

    django shell

---
+++ python_stack/django/django_orm/lectures/django_orm/apps/main/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add="True")
+    update_at = models.DateTimeField(auto_now="True")
