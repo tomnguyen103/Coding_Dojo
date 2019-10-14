5b34b3baff3ce8aaa3966d91ecde5ffc5b919d2b
tomnguyen103 <huuthong103@gmail.com>
2019-10-14 12:07:25 -0700

    Finished Dojo_Ninjas Assignment

--- python_stack/django/django_orm/dojo_ninjas_proj/apps/dojo_ninjas_app/models.py
+++ python_stack/django/django_orm/dojo_ninjas_proj/apps/dojo_ninjas_app/models.py
@@ -5,6 +5,7 @@ class Dojo(models.Model):
     name = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     state = models.CharField(max_length=2)
+    desc = models.TextField()
 
 class Ninja(models.Model):
     first_name = models.CharField(max_length=255)
