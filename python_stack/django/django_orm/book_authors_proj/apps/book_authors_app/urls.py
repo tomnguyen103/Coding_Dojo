da7da05e96d5a7480918ab2ff522c19a1ca6637a
tomnguyen103 <huuthong103@gmail.com>
2019-10-14 17:10:53 -0700

    finished Book/Authors with template

--- python_stack/django/django_orm/book_authors_proj/apps/book_authors_app/urls.py
+++ python_stack/django/django_orm/book_authors_proj/apps/book_authors_app/urls.py
@@ -4,7 +4,10 @@ from . import views
 urlpatterns = [
     url(r'^$', views.addBooks),
     url(r'^process', views.process),
-    url(r'^book/(?P<id>\d+)$', views.book),
-    url(r'^authors$', views.authors),
-    url(r'^process$',views.process),
+    url(r'^books/(?P<number>\d+)$', views.book),
+    url(r'^add_author/(?P<number>\d+)$', views.addAuthor),
+    url(r'^authors$', views.addAuthors),
+    url(r'^process2$',views.process2),
+    url(r'^authors/(?P<number>\d+)$', views.author),
+    url(r'^add_book/(?P<number>\d+)$', views.addBook),
 ]
--- python_stack/django/django_orm/book_authors_proj/apps/book_authors_app/views.py
+++ python_stack/django/django_orm/book_authors_proj/apps/book_authors_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 
-from .models import Book
+from .models import *
 
 # Create your views here.
 def addBooks(request):
@@ -15,11 +15,63 @@ def process(request):
 
     return redirect("/")
 
-def book(request,id):
+def book(request,number):
 
-    return render(request,'book_authors_app/bookdetails.html')
+    book = Book.objects.get(id=int(number))
+    book_author = book.authors.all()
+    all_authors = Author.objects.all()
+    context = {
+        "book_html" : book,
+        "book_author_html" : book_author,
+        "all_authors_html" : all_authors,
+    }
 
-def authors(request):
-    return render(request,'book_authors_app/authors.html',{
-        "bookLists": Book.objects.all()
+    return render(request,'book_authors_app/bookdetails.html',context)
+
+def addAuthor(request,number):
+    book_id = int(number)
+    author_id = int(request.POST["author"])
+
+    get_book_id = Book.objects.get(id=book_id)
+    get_author_id = Author.objects.get(id=author_id)
+
+    get_book_id.authors.add(get_author_id)
+
+    return redirect(f"/books/{number}")
+
+def addAuthors(request):
+    return render(request, 'book_authors_app/authors.html',{
+        "authorLists": Author.objects.all()
     })
+
+
+def process2(request):
+    form = request.POST
+
+    Author.objects.create(first_name=form["first_name"], last_name=form["last_name"], notes=form["notes"])
+
+    return redirect("/")
+
+def author(request,number):
+
+    author = Author.objects.get(id=int(number))
+    author_book = author.books.all()
+    all_books = Book.objects.all()
+    context = {
+        "author_html" : author,
+        "author_book_html" : author_book,
+        "all_books_html" : all_books,
+    }
+
+    return render(request,'book_authors_app/authordetails.html',context)
+
+def addBook(request,number):
+    author_id = int(number)
+    book_id = int(request.POST["book"])
+
+    get_author_id = Author.objects.get(id=author_id)
+    get_book_id = Book.objects.get(id=book_id)
+
+    get_author_id.books.add(get_book_id)
+
+    return redirect(f"/authors/{number}")
