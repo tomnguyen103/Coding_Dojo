786f6c3c8b651ab46180994a43de105ace9027e5
tomnguyen103 <huuthong103@gmail.com>
2019-10-14 20:21:15 -0700

    work like magic after 1hr

--- python_stack/django/django_full_stack/TVShows/apps/main/urls.py
+++ python_stack/django/django_full_stack/TVShows/apps/main/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^shows$', views.index),
+    url(r'^$',views.index),
+    url(r'^shows$',views.shows),
+    url(r'^shows/new$',views.new_shows),
+    url(r'^shows/new/add$',views.add_new_shows),
+    url(r'^shows/(?P<show_id>\d+)$',views.display_show),
 ]
--- python_stack/django/django_full_stack/TVShows/apps/main/views.py
+++ python_stack/django/django_full_stack/TVShows/apps/main/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render,redirect,HttpResponse
+from .models import *
 
 # Create your views here.
 def index(request):
-    return render(request,'main/index.html')
+    return redirect("/shows")
+
+def shows(request):
+    return render(request,'main/index.html',{
+        "all_shows_html" : Show.objects.all()
+    })
+def new_shows(request):
+
+    return render(request,"main/create.html")
+
+def add_new_shows(request):
+    form = request.POST
+    print("you are right at form")
+    new_show = Show.objects.create(title=form['title'],network=form['network'],release_date=form['release_date'],desc=form['desc'])
+    return redirect("/shows")
+
+def display_show(request):
+    pass
