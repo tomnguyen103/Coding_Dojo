285545e2e07142f9aabe983c75a41319a5ac999f
tomnguyen103 <huuthong103@gmail.com>
2019-11-01 14:00:54 -0700

    updated

--- MEAN/Express/Counter/server.js
+++ MEAN/Express/Counter/server.js
@@ -2,29 +2,45 @@ const express = require('express');
 const session = require('express-session');
 
 const app = express();
-var count = 0;
 
 app.use(express.static(__dirname+'/static'));
 
 app.use(session({
     secret: 'somethinghere',
-    // resave: false,
-    // saveUninitialized: true,
-    // cookie: {maxAge: 60000},
+    resave: false,
+    saveUninitialized: true,
+    cookie: {maxAge: 60000},
 }))
+
+app.use(express.urlencoded({extended: true}));
+
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname+'/views');
 
 app.get('/',(req,res)=> {
-    count++;
-    console.log(count)
-    res.render('index', (req,res)=> {count: count})
-    });
+    if(req.session.visit === undefined){
+        req.session.visit = 0;
+    }
+    req.session.visit += 1;
+    // console.log(count);
+    res.render('index', {count : req.session.visit});
+})
 
-app.post('/process-name',(req,res)=>{
-    // console.log("this is the body "+req.body);
-    req.session.name = req.body.name;
+
+// app.post('/process-time',(req,res)=>{
+//     // console.log("this is the body "+req.body);
+//     req.session = req.body;
+//     res.redirect('/');
+// })
+
+app.post('/reset', (req,res)=>{
+    req.session.visit = 0;
+    res.redirect('/');
+});
+
+app.post('/add2', (req,res)=>{
+    req.session.visit += 1;
     res.redirect('/');
 })
 
