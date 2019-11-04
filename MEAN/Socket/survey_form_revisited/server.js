989e7dfebe3fc62da59473ea5dde4e3ca8a2b03f
tomnguyen103 <huuthong103@gmail.com>
2019-11-03 17:08:33 -0800

    updated

--- MEAN/Socket/survey_form_revisited/server.js
+++ MEAN/Socket/survey_form_revisited/server.js
@@ -3,25 +3,21 @@ const session = require('express-session');
 
 const app = express();
 
-app.use(express.static(__dirname+'/static'));
-
-app.use(session({
-    secret: "somesecretkeyhere",
-}));
-
+app.use(session({secret: "somesecretkey"}));
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname+'/views');
-
 app.use(express.urlencoded({extended: true}));
 
-app.get('/',(req,res)=>{
+app.use(express.static(__dirname+'/public'));
+
+app.get('/',(req,res)=> {
     if(req.session.visit === undefined){
         req.session.visit = 1;
     }
     req.session.visit += 1;
-    res.render('index');
-})
+    res.render('index')
+});
 
 app.post('/result',(req,res)=>{
     req.session.name = req.body.name;
@@ -29,13 +25,15 @@ app.post('/result',(req,res)=>{
     req.session.fav_language = req.body.fav_language;
     req.session.comment = req.body.comment;
 
-    res.render('result',{
+    res.render('result', {
         visit: req.session.visit,
         name: req.session.name,
         location: req.session.location,
         fav_language: req.session.fav_language,
         comment: req.session.comment,
     });
-})
+});
 
-app.listen(3000, ()=> console.log('Listening on port 3000'));
+app.listen(3000,()=> console.log('Listening port 3000'));
+
+// const io = require('socket.io')(server);
