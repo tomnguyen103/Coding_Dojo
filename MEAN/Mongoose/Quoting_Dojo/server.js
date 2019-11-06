64c27ea8e6b76740524421d1304f619d9cd1853b
tomnguyen103 <huuthong103@gmail.com>
2019-11-06 13:50:19 -0800

    start mongoose

---
+++ MEAN/Mongoose/Quoting_Dojo/server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+mongoose.connect('mongodb://localhost/quoting_dojo',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const QuoteSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: [
+            true,
+            'Please enter your name!',
+        ]
+    },
+    quote:{
+        type: String,
+        required: [
+            true,
+            'Please enter your quote!',
+        ]
+    }
+},{timestamps: true});
+
+const Quote = mongoose.model('Quote', QuoteSchema);
+
+app.use(express.static(__dirname+'/static'));
+
+app.use(express.urlencoded({ extended: true}));
+
+app.set('view engine', 'ejs');
+
+app.set('views',__dirname+'/views');
+
+app.use(express.urlencoded({extended: true}));
+
+
+app.listen(1223,()=> console.log('Listening on port 1223'));
+
+app.get('/',(req,res)=>{
+    Quote.create(req.body)
+    .then(console.log)
+    .catch(console.log)
+    .finally( ()=>{
+        res.render('index')
+    })
+    // res.render('index');
+});
+
