315319da2c83d3ba00e84f73388ab7dba9ff9941
tomnguyen103 <huuthong103@gmail.com>
2019-11-11 17:00:37 -0800

    started Restful Tasks

--- MEAN/Angular/Restful_Task_API_Angular/public/src/app/app.component.ts
+++ MEAN/Angular/Restful_Task_API_Angular/public/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpService } from './http.service';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   // title = 'public';
   title = 'MEAN';
+  
+  constructor(private _httpService: HttpService){
+
+  }
 }
--- MEAN/Angular/Restful_Task_API_Angular/public/src/app/app.module.ts
+++ MEAN/Angular/Restful_Task_API_Angular/public/src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+import { HttpService } from './http.service';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
@@ -10,9 +15,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
