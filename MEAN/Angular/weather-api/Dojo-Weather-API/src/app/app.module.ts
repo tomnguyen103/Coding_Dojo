26004a8f4bb62be93e8c4b59407b911ccfccfc04
tomnguyen103 <huuthong103@gmail.com>
2019-11-18 10:36:48 -0800

    updated

--- MEAN/Angular/weather-api/Dojo-Weather-API/src/app/app.module.ts
+++ MEAN/Angular/weather-api/Dojo-Weather-API/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { CityComponent } from './city/city.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    CityComponent
   ],
   imports: [
     BrowserModule,
--- MEAN/Angular/weather-api/Dojo-Weather-API/src/app/http.service.ts
+++ MEAN/Angular/weather-api/Dojo-Weather-API/src/app/http.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  getWeather(callback, city: City){
+
+  }
 }
