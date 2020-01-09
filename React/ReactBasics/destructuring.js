fa0f6e533e54382f70ffaf3a3795bf2946d0245c
tomnguyen103 <huuthong103@gmail.com>
2020-01-09 13:28:57 -0800

    updated

---
+++ React/ReactBasics/destructuring.js
@@ -0,0 +1,11 @@
+const tom ={
+    firstName: "Tom",
+    lastName: "Nguyen",
+    email: "t@gmail.com"
+}
+// const tomEmail = tom["email"];
+// console.log(tomEmail);
+
+const { lastName } = tom;
+
+console.log(lastName);
